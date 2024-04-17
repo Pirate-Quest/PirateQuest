d3460b79ba1ae4c0d1f7a11ac20fd727d3ff25f0
William Jolivet <william.jolivet@epitech.eu>
2024-04-17 16:00:31 +0200

    [add] project structure

--- include/render.h
+++ include/render.h
@@ -8,4 +8,35 @@
 #ifndef RENDER_H_
     #define RENDER_H_
 
+    #include <SFML/Graphics.h>
+
+typedef struct resolution_s {
+    int width;
+    int height;
+} resolution_t;
+
+typedef struct camera_s {
+    sfView *view;
+    sfVector2f center;
+    sfVector2f size;
+} camera_t;
+
+// render/camera.c
+void set_camera_center(camera_t *camera, sfVector2f center);
+void set_camera_size(camera_t *camera, sfVector2f size);
+void set_camera(camera_t *camera, sfRenderWindow *window);
+
+typedef struct render_window_s {
+    sfRenderWindow *window;
+    camera_t *camera;
+} render_window_t;
+
+// render/render_window.c
+resolution_t get_resolution(int index);
+int get_resolutions_count(void);
+sfVideoMode get_sfvideo_mode(int index);
+
+// render/render_window.c
+void init_icon(render_window_t *window);
+
 #endif /* !RENDER_H_ */
--- src/main.c
+++ src/main.c
@@ -6,8 +6,60 @@
 */
 
 #include <SFML/Graphics.h>
+#include <stdlib.h>
+#include "../include/commons.h"
+#include "../include/render.h"
+#include "../include/event.h"
+
+void free_window(render_window_t *window)
+{
+    if (window == NULL)
+        return;
+    if (window->camera != NULL) {
+        if (window->camera->view != NULL)
+            sfView_destroy(window->camera->view);
+        free(window->camera);
+    }
+    sfRenderWindow_destroy(window->window);
+    free(window);
+}
+
+render_window_t *init_window(void)
+{
+    render_window_t *window = malloc(sizeof(render_window_t));
+
+    if (window == NULL) {
+        my_puterr("Error: Could not allocate memory for window.\n");
+        return NULL;
+    }
+    window->window = sfRenderWindow_create(
+        get_sfvideo_mode(0), "Pirate Quest", sfResize | sfClose, NULL);
+    window->camera = malloc(sizeof(camera_t));
+    window->camera->view = sfView_create();
+    window->camera->center = (sfVector2f){0, 0};
+    window->camera->size = (sfVector2f){1920, 1080};
+    set_camera(window->camera, window->window);
+    return window;
+}
 
 int main(void)
 {
-    return 0;
+    pirate_quest_t game;
+
+    game.window = init_window();
+    if (game.window == NULL)
+        return my_puterr("Error: Could not allocate memory for window.\n");
+    if (game.window->window == NULL) {
+        free_window(game.window);
+        return my_puterr("Error: Could not create window. This could be "
+            "due to a lack of memory. Or incompatible hardware.\n");
+    }
+    init_icon(game.window);
+    while (sfRenderWindow_isOpen(game.window->window)) {
+        update_listeners(&game);
+        sfRenderWindow_clear(game.window->window, sfBlack);
+        sfRenderWindow_display(game.window->window);
+    }
+    free_window(game.window);
+    return FALSE;
 }

851befab73bdc2264d4caa9ccd4c9620264e826e
MaxxRobert <max.robert@epitech.eu>
2024-05-24 15:07:26 +0200

    [ADD] zone musique

--- src/music/music_player.c
+++ src/music/music_player.c
@@ -14,7 +14,9 @@
 const music_registry_t music_registry[] = {
     {MENU_MUSIC, "Menu", "assets/music/menu.wav"},
     {TUTO_MUSIC, "Tuto", "assets/music/tuto.wav"},
-    {TAVERN_MUSIC, "Tavern", "assets/music/tavern.wav"}
+    {TAVERN_MUSIC, "Tavern", "assets/music/tavern.wav"},
+    {DESERT_MUSIC, "Desert", "assets/music/desert.wav"},
+    {BOSS_MUSIC, "Boss", "assets/music/boss.wav"}
 };
 
 void init_musique(pirate_quest_t *game)
@@ -34,5 +36,18 @@ void music_player(pirate_quest_t *game, int music)
     for (int i = 0; i < TOTAL_MUSIC; i++){
         sfMusic_stop(game->music->musics[i]);
     }
+    game->player->data->phase = music;
     sfMusic_play(game->music->musics[music]);
+}
+
+void select_music(pirate_quest_t *game)
+{
+    if (player_is_in_square_rect(game, (sfVector2i){134, 203}, (sfVector2i){136, 205}) && game->player->data->phase != TUTO_MUSIC)
+        music_player(game, TUTO_MUSIC);
+    if (player_is_in_square_rect(game, (sfVector2i){134, 237}, (sfVector2i){136, 239}) && game->player->data->phase != DESERT_MUSIC)
+        music_player(game, DESERT_MUSIC);
+    if (player_is_in_square_rect(game, (sfVector2i){39, 239}, (sfVector2i){41, 241}) && game->player->data->phase != DESERT_MUSIC)
+        music_player(game, DESERT_MUSIC);
+    if (player_is_in_square_rect(game, (sfVector2i){39, 225}, (sfVector2i){41, 227}) && game->player->data->phase != BOSS_MUSIC)
+        music_player(game, BOSS_MUSIC);
 }
